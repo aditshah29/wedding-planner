8ed9bf5402ceedc461b59edd8f911101b5c01193
aditshah29 <aditshah294@gmail.com>
2026-07-17 08:06:32 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        services: resolve(__dirname, 'services.html'),
+        gallery: resolve(__dirname, 'gallery.html'),
+        packages: resolve(__dirname, 'packages.html'),
+        appointment: resolve(__dirname, 'appointment.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      },
+    },
+  },
+});
